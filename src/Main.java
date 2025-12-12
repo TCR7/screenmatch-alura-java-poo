6cce440fc3a036735db0ba7a8cd6e983b270a96a
Thiago Campanille <thiago.reis@amazonasinovare.com.br>
2025-12-12 18:24:24 -0300

    feat: :sparkles:  added methods and attribute for a new movie

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        Movie newMovie = new Movie();
+        newMovie.name = "O Poderoso Chefão";
+        newMovie.releaseYear = 1972;
+        newMovie.durationMinutes = 175;
+
+        newMovie.showData();
+
+        newMovie.review(8.5);
+        newMovie.review(9.0);
+        newMovie.review(5);
+
+        System.out.println(newMovie.ratingSums);
+        System.out.println(newMovie.totalReviews);
+        System.out.println(newMovie.avarageRating());
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,23 @@
 public class Movie {
+    String name;
+    int releaseYear;
+    boolean includedInSubscription;
+    double ratingSums;
+    int totalReviews;
+    int durationMinutes;
+
+    void showData(){
+        System.out.println("Nome do filme: " + name);
+        System.out.println("Ano de lançamento: " + releaseYear);
+        System.out.println("Duração do filme em minutos: " + durationMinutes);
+    }
+
+    void review(double entry){
+        ratingSums += entry;
+        totalReviews++;
+    }
+
+    double avarageRating(){
+        return ratingSums / totalReviews;
+    }
 }
